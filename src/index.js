277456bc06225ed7a9afb47af67cdd9129a464e7
AlexanderKrasovskiy <fuxik2007@gmail.com>
2022-01-25 11:57:32 +0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 === 1) return false;
+
+  const config = Object.fromEntries(bracketsConfig);
+  let stack = [];
+
+  for (let char of str) {
+    let stackTop = stack[stack.length - 1];
+    if (char === config[stackTop]) {
+      stack.pop();
+    } else {
+      stack.push(char)
+    }
+  };
+
+  return stack.length === 0
 }
